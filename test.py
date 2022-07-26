edbd5601f2dfc7da79368b16ac69e5449b92a92e
Arno Simons <arno.simons@gmail.com>
2022-07-26 17:14:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 mysum = 4 + 3
 import matplotlib.pyplot as plt
 class Session:
-  def __init__(numbers):
+  def __init__(self, numbers):
     self.fig = plt.gcf()
     self.ax = plt.gca()
     self.ax.plot(numbers)
