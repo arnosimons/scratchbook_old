790187143c9112bdd5736efbbbe5f4cf7ab0d199
arno.simons <arno.simons@posteo.de>
2022-08-04 13:59:00 +0200

    update files

--- classes_and_functions.py
+++ classes_and_functions.py
@@ -74,6 +74,9 @@ class Scratch:
         return Scratch([[[i[0][0], i[0][1], i[0][2] + n], i[1], i[2], i[3]] for i in self.slices])
     
     ys = yshift
+
+    def __pow__(self, n): # yshift
+        return Scratch([[[i[0][0], i[0][1], i[0][2] + n], i[1], i[2], i[3]] for i in self.slices])
     
 class Session:
     
