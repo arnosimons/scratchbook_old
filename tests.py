efb45c12f9675bb34b30dc53a72e993d1d63fd7b
arno.simons <arno.simons@posteo.de>
2022-09-01 21:17:28 +0200

    update files

--- tests.py
+++ tests.py
@@ -8,9 +8,7 @@ codebook = {
     "tazer1":"~bLog_f1Log_12",
 }
 myscratch = makeScratch(
-  "stab + i + (f2Ex//1)**.2 + ~(f2Log//1)**.2 + tazer1",
-  codebook
-  )
+  "stab + i + (f2Ex//1)**.2 + ~(f2Log//1)**.2 + tazer1" )
 info = getInfo(myscratch)
 fig = Session(myscratch, fontsize=11, w_pad=2).fig
 # fig.show()
